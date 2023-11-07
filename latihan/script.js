1f6b2e00429c5842f643df3bd61a22c7a22faf24
Immanuel <112744859+ImmnlBayuAgstns@users.noreply.github.com>
2023-11-07 14:50:37 +0700

    Add files via upload

---
+++ latihan/script.js
@@ -0,0 +1,9 @@
+let nilai = prompt("masukkan nilai: ")
+
+if (nilai >= 80 && nilai <= 100) {
+    alert("memenuhi syarat untuk lulus dan mendapatkan ijazah")
+    document.write("CONGRATSSS!!")
+} else {
+    alert("mengulang tahun depan")
+    document.write("JIAKKKHHHH NGULANG!!")
+}
